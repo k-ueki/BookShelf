035511e53f4a8e7699ccbff660db9fcebf12f71e
k-ueki <k.ueki@rb4.so-net.ne.jp>
2019-10-22 11:00:10 +0900

    fix

--- src/pages/community/detail.vue
+++ src/pages/community/detail.vue
@@ -23,13 +23,15 @@
 
             <div class="parsonal-sp">
 				<img class="iconSelf" src="../../../images/library-1147815_1920.jpg"> 
-				<div class="parsonal-name"> {{ name }}</div>
-				<div class="">{{userid}}</div>
+				<div class="com-name" style="font-size:25;"> {{ name }} </div>
 
 				<br/>
 				<span style="color:rgba(0,0,0,0.4);">members</span>
 				<div v-for="mem in members">
 					<div>
+						<a>
+							<!-- <img class="mem&#45;icon" :src="mem.photo_url">  -->
+						</a>
 						<a>
 							{{mem.name}}
 						</a>
@@ -236,5 +238,12 @@ export default{
 }
 .modalHeader{
 	align:right;
+}
+
+.mem-icon{
+	width:30px;;
+	height:30px;
+	border-radius:50%;
+	margin-top:10px;
 }
 </style>
